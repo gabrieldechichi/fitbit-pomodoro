75f6275eda148b5042f6aab931f018997519b173
Gabriel <gabrieldechichi@gmail.com>
2021-01-09 18:43:40 -0700

    More pomodoro tests

--- test/pomoevents/components/pomodoro.test.ts
+++ test/pomoevents/components/pomodoro.test.ts
@@ -3,7 +3,6 @@ import { Clock, ClockGranularity, ClockTickEvent } from '../../../app/fitbit-mod
 import { Logger } from 'ts-log';
 import { createMock } from "ts-auto-mock"
 import { On, method } from "ts-auto-mock/extension"
-import { EventEmitter } from '../../../app/common/eventEmitter';
 import { Delegate } from '../../../app/common/delegate';
 
 let pomodoro: Pomodoro = null
@@ -31,26 +30,26 @@ afterAll(() => {
     global.Date.now = realDate
 })
 
-describe('initialization', () => {
-    test('should subcribe to clock', () => {
+describe('initialization should', () => {
+    test('subcribe to clock', () => {
         const mockedMethod = On(clock).get(method(c => c.registerClockCallback))
         expect(mockedMethod).toHaveBeenCalled()
     })
 })
 
-describe('state control', () => {
+describe('state control should', () => {
     test('begins in Idle state', () => {
         expect(pomodoro.getState()).toBe(PomodoroState.Idle)
         // const mockMethod = On(clock).get(method(mock => mock['onTick']))
     })
 
-    test('switches from Idle to Working', () => {
+    test('switch from Idle to Working', () => {
         expect(pomodoro.getState()).toBe(PomodoroState.Idle)
         pomodoro.start()
         expect(pomodoro.getState()).toBe(PomodoroState.Working)
     })
 
-    test('switches from Working to Idle', () => {
+    test('switch from Working to Idle', () => {
         pomodoro.start()
         expect(pomodoro.getState()).toBe(PomodoroState.Working)
         pomodoro.stop()
@@ -58,10 +57,12 @@ describe('state control', () => {
     })
 })
 
-describe('state transition', () => {
+describe('state transition should', () => {
     let delegate: Delegate<Date> = null
+    let now: number
 
     beforeAll(() => {
+        global.Date.now = () => now
         delegate = new Delegate()
         const mockedMethod = On(clock).get(method(c => c.registerClockCallback))
         mockedMethod.mockImplementation(jest.fn((callback: ClockTickEvent) => {
@@ -76,14 +77,15 @@ describe('state transition', () => {
         mockedMethod.mockClear()
     })
 
+    beforeEach(() => {
+        now = realDate()
+    })
+
     afterEach(() => {
         delegate = new Delegate()
     })
 
     test('switch to Resting when Working is done', () => {
-        let now = realDate()
-        global.Date.now = () => now
-
         pomodoro.start()
         expect(pomodoro.getState()).toBe(PomodoroState.Working)
 
@@ -94,5 +96,52 @@ describe('state transition', () => {
         now += pomodoroSettings.workTimeSeconds * 1000 * 0.5
         delegate.emit(null)
         expect(pomodoro.getState()).toBe(PomodoroState.Resting)
+    })
+
+    test('switch to Working when ShortBreak is done', () => {
+        pomodoro.start()
+        pomodoro.skip()
+        expect(pomodoro.getState()).toBe(PomodoroState.Resting)
+
+        now += pomodoroSettings.shortBreakTimeSeconds * 1000 * 0.5
+        delegate.emit(null)
+        expect(pomodoro.getState()).toBe(PomodoroState.Resting)
+
+        now += pomodoroSettings.shortBreakTimeSeconds * 1000 * 0.5
+        delegate.emit(null)
+        expect(pomodoro.getState()).toBe(PomodoroState.Working)
+    })
+
+    test('switch to Work after Long Break is done', () => {
+        pomodoro.start()
+        while (!pomodoro.isInLongBreak()) {
+            pomodoro.skip()
+        }
+
+        now += pomodoroSettings.longBreakTimeSeconds * 1000 * 0.5
+        delegate.emit(null)
+        expect(pomodoro.getState()).toBe(PomodoroState.Resting)
+        expect(pomodoro.isInLongBreak()).toBe(true)
+
+        now += pomodoroSettings.longBreakTimeSeconds * 1000 * 0.5
+        delegate.emit(null)
+        expect(pomodoro.getState()).toBe(PomodoroState.Working)
+    })
+
+    test('switch to LongBreak after enough work session', () => {
+        pomodoro.start()
+        expect(pomodoro.getState()).toBe(PomodoroState.Working)
+        for (let i = 0; i <= pomodoroSettings.numberOfSessionsBeforeBreak; i++) {
+            now += pomodoroSettings.workTimeSeconds * 1000
+            delegate.emit(null)
+
+            if (i < pomodoroSettings.numberOfSessionsBeforeBreak) {
+                now += pomodoroSettings.shortBreakTimeSeconds * 1000
+                delegate.emit(null)
+            }
+        }
+
+        expect(pomodoro.getState()).toBe(PomodoroState.Resting)
+        expect(pomodoro.isInLongBreak()).toBe(true)
     })
 })
